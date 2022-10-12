ca092076d6343a16ecae0778012f26948db752f9
Miguel SE <masemmiguel@gmail.com>
2022-10-12 11:20:29 -0400

    connected express to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React from "react"; 
+
 import './App.css';
 import Chart1 from './Components/Chart1';
 import MainButton from './Components/MainButton';
@@ -7,42 +9,59 @@ import BasicButton from './Components/BasicButton';
 let test1 = [1,2,3,4,5,6,7,8];
 let test2 = [1,2,3,4,5,6,9,6];
 
-function testingData(){
-  let avg1 = 0;
-  let avg2 = 0;
-  avg1 =  test1.forEach((prev, curr) => 
-      prev + curr    
-  , avg1);
-  avg2 =  test2.forEach((prev, curr) => 
-      prev + curr    
-  , avg2);
-  if(avg1 === avg2) {
-      return <h3> Suceeded SanityCheck</h3>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
   }
-  return <h3> failed SanityCheck </h3>
-}
 
-function App() {
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+  }
 
-  return (
-    <div>
-      <h1>Very Early UI Prototype</h1>
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  testingData(){
+    let avg1 = 0;
+    let avg2 = 0;
+    avg1 =  test1.forEach((prev, curr) => 
+        prev + curr    
+    , avg1);
+    avg2 =  test2.forEach((prev, curr) => 
+        prev + curr    
+    , avg2);
+    if(avg1 === avg2) {
+        return <h3> Suceeded SanityCheck</h3>
+    }
+    return <h3> failed SanityCheck </h3>
+  }
+
+  render() {
+    return(
       <div>
-        <MainButton 
-            startButton = {"GoToAnaylisis"} 
-            backButton = {"GoBackToSanityCheck"} 
-            unPressedComponent = {() => <div><BasicButton name = "SanityCheck" clickMethod = {testingData}/></div>} 
-            pressedComponent = {() =>  
-                <div>
-                    <ButtonLists name = "hi2"/>
-                    <BasicButton name = "testFiltered" clickMethod = {() => <Chart1/>}/>
-                </div>}
-        />
-        {/* <ButtonLists buttonList name = "hi"/> */}
-        <BasicButton name = "closeProgram" clickMethod = {() => console.log("Under Construction")}/>
+        <h1>Very Early UI Prototype</h1>
+        <p className="App-intro">{this.state.apiResponse}</p>
+        <div>
+          <MainButton 
+              startButton = {"GoToAnaylisis"} 
+              backButton = {"GoBackToSanityCheck"} 
+              unPressedComponent = {() => <div><BasicButton name = "SanityCheck" clickMethod = {this.testingData}/></div>} 
+              pressedComponent = {() =>  
+                  <div>
+                      <ButtonLists name = "hi2"/>
+                      <BasicButton name = "testFiltered" clickMethod = {() => <Chart1/>}/>
+                  </div>}
+          />
+          {/* <ButtonLists buttonList name = "hi"/> */}
+          <BasicButton name = "closeProgram" clickMethod = {() => console.log("Under Construction")}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
