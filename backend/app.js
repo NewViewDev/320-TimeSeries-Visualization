123fff52044208f4a3af62e9223ffac7c301d6b8
Omer <otaniyici@umass.edu>
2022-10-30 21:04:54 -0400

    add cors package

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 require("dotenv").config();
 
 const prisma = require("./utils/prisma");
