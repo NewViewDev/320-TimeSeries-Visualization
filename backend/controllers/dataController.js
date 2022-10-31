a052d2d60b0341808e5d75cfc6f5a321c8834103
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-10-31 11:38:21 -0400

    added option for user to select specific field for a node and added basic error handling

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -4,33 +4,49 @@ const prisma = require("../utils/prisma");
 const Node = require("../models/Node");
 
 exports.getNodeNames = async (req, res) => {
-  let nodes = await prisma.nodes.findMany({
-    select: {
-      PNODE_NAME: true,
-    },
-    distinct: ["PNODE_NAME"],
-  });
-  nodes = nodes.map((node) => node.PNODE_NAME);
-  res.status(StatusCodes.OK).json({ data: { nodes } });
+	let nodes = await prisma.nodes.findMany({
+		select: {
+			PNODE_NAME: true,
+		},
+		distinct: ["PNODE_NAME"],
+	});
+	nodes = nodes.map((node) => node.PNODE_NAME);
+	res.status(StatusCodes.OK).json({ data: { nodes } });
 };
 
 exports.getScenarios = async (req, res) => {
-  let scenarios = await prisma.scenarios.findMany({});
-  scenarios = scenarios.map((s) => ({
-    SCENARIO_ID: s.SCENARIO_ID,
-    SCENARIO_NAME: s.SCENARIO_NAME,
-  }));
-  res.status(StatusCodes.OK).json({ data: { scenarios } });
+	let scenarios = await prisma.scenarios.findMany({});
+	scenarios = scenarios.map((s) => ({
+		SCENARIO_ID: s.SCENARIO_ID,
+		SCENARIO_NAME: s.SCENARIO_NAME,
+	}));
+	res.status(StatusCodes.OK).json({ data: { scenarios } });
 };
 
 exports.getNode = async (req, res) => {
-  console.log(req.query);
-  const { PNODE_NAME, SCENARIO_ID_1, SCENARIO_ID_2 } = req.query;
-  const nodes = await prisma.nodes.findMany({
-    where: {
-      PNODE_NAME: PNODE_NAME,
-      OR: [{ SCENARIO_ID: SCENARIO_ID_1 }, { SCENARIO_ID: SCENARIO_ID_2 }],
-    },
-  });
-  res.status(StatusCodes.OK).json({ data: { nodes } });
+	console.log(req.query);
+	const { PNODE_NAME, SCENARIO_ID_1, SCENARIO_ID_2, FIELD } = req.query;
+	let nodes;
+
+	try {
+		nodes = await prisma.nodes.findMany({
+			where: {
+				PNODE_NAME: PNODE_NAME,
+				OR: [
+					{ SCENARIO_ID: SCENARIO_ID_1 },
+					{ SCENARIO_ID: SCENARIO_ID_2 },
+				],
+			},
+			select: {
+				SCENARIO_ID: true,
+				[FIELD]: true,
+			},
+		});
+	} catch (err) {
+		res.status(StatusCodes.BAD_REQUEST).json({
+			message: err.message,
+		});
+	}
+
+	res.status(StatusCodes.OK).json({ data: { nodes } });
 };
