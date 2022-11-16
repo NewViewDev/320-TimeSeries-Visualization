0c7e6abf75b55c366a8b3750ad090e0c98d3c85e
Miguel SE <masemmiguel@gmail.com>
2022-11-16 12:41:10 -0500

    stuff

--- client/src/NewPages/SanityCheck.js
+++ client/src/NewPages/SanityCheck.js
@@ -5,6 +5,8 @@ import Container from "../CustomComponents/Container";
 import Button from "../CustomComponents/Button";
 import Navbar from "../CustomComponents/Navbar";
 import NavButton from "../CustomComponents/NavbarButton";
+import NavElement from "../CustomComponents/NavbarElement"
+import Scatterplot from "../Components/Scatterplot";
 
 class SanityCheckPage extends React.Component {
   constructor(props) {
@@ -24,12 +26,15 @@ class SanityCheckPage extends React.Component {
         <Button className="action">Check</Button>
         <br/><br/>
         <Container className="light">
-          <Navbar className="grey" height="50px">
+          <Navbar height="50px">
             <NavButton onClick={() => {this.setPage(1)}} active={this.state.page === 1}>Scatter</NavButton>
             <NavButton onClick={() => {this.setPage(2)}} active={this.state.page === 2}>Histogram</NavButton>
             <NavButton onClick={() => {this.setPage(3)}} active={this.state.page === 3}>Heat Map</NavButton>
+            <NavElement float="right">
+              <Dropdown className="dark" list={[]}>Select PNode</Dropdown>
+            </NavElement>
           </Navbar>
-          <Dropdown list={[]}>Select PNode</Dropdown>
+          <Scatterplot data={{}}>Select PNode</Scatterplot>
         </Container>
       </>
     );
