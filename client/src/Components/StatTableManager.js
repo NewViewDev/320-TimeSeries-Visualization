9db035487d2162514ec2f7afa031318380d00fa2
wzh24 <wxzheng16@gmail.com>
2022-12-17 21:01:17 -0500

    Fixed some syntax that was causing issues

--- client/src/Components/StatTableManager.js
+++ client/src/Components/StatTableManager.js
@@ -1,26 +1,9 @@
 import React from "react"; 
-
-
 import AnalyticsTable from "../Components/AnalyticsTable";
 
 export const rows = [];
 
 let clearArray = () => { rows.length = 0; }
-
-function genFetch2(ScenarioID, Interval, minuteOffset, StartDate, EndDate){
-    let offset = minuteOffset/60
-    let startID = extractPeriod(StartDate)
-    let endID = extractPeriod(EndDate)
-    let toFetch = 'http://localhost:4000/api/v1/data/nodes/group'
-    toFetch += '?SCENARIO_ID=' + ScenarioID; //1
-    // toFetch += '&START_DATE=2020-07-01T01:00:00'
-    toFetch += '&START_DATE='+ startID
-    // toFetch += '&END_DATE=2020-12-03T01:00:00'
-    toFetch += '&END_DATE='+endID
-    toFetch += '&FIELD=LMP&INTERVAL=daily'
-    toFetch += '&OFFSET=' + offset;
-    return toFetch;
-}
 
 //Generates 1 row
 function genRow(timeperiod, mean, std, median, keyValue){
